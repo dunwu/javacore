4e29187890acb5e801b3d30142bb0ebcd7602383
Zhang Peng <forbreak@163.com>
2018-01-17 14:18:09 +0800

    :tada: 添加 spring-notes-oop

---
+++ codes/oop/src/main/java/io/github/dunwu/javase/access/Operate.java
@@ -0,0 +1,19 @@
+package io.github.dunwu.javase.access;
+
+public class Operate {
+    public static int add(int i, int j) {
+        return i + j;
+    }
+
+    public static int sub(int i, int j) {
+        return i - j;
+    }
+
+    public static int mul(int i, int j) {
+        return i * j;
+    }
+
+    public static int div(int i, int j) {
+        return i / j;
+    }
+};
