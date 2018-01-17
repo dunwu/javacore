3ad13b1e4b0b2f553e3160024b21ead15bdd2ceb
Zhang Peng <forbreak@163.com>
2018-01-17 15:03:21 +0800

    :sparkles: 添加面向对象示例

--- codes/oop/src/main/java/io/github/dunwu/javase/object/Person2.java
+++ codes/oop/src/main/java/io/github/dunwu/javase/object/Person2.java
@@ -6,6 +6,8 @@ package io.github.dunwu.javase.object;
 public class Person2 {
     private String name; // 声明姓名属性
     private int age; // 声明年龄属性
+    private Book book;
+    private Person2 child;
 
     public Person2() {
     }
@@ -13,6 +15,10 @@ public class Person2 {
     public Person2(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    void fun(Person2 obj) { // 接收本类的引用
+        obj.age = 18;// 直接通过对象调用本类的私有属性
     }
 
     public void setName(String n) { // 设置姓名
@@ -31,6 +37,22 @@ public class Person2 {
 
     public int getAge() { // 取得年龄
         return age;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public void setChild(Person2 c) {
+        child = c;
+    }
+
+    public Person2 getChild() {
+        return child;
     }
 
     public void tell() {
