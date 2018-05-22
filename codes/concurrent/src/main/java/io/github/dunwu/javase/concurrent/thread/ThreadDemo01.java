38f7d2d6055ffe224c5e468525c4481a6c44ad8e
Zhang Peng <forbreak@163.com>
2018-05-22 10:37:02 +0800

    :bookmark: 创建线程示例

--- codes/concurrent/src/main/java/io/github/dunwu/javase/concurrent/thread/ThreadDemo01.java
+++ codes/concurrent/src/main/java/io/github/dunwu/javase/concurrent/thread/ThreadDemo01.java
@@ -5,30 +5,32 @@ package io.github.dunwu.javase.concurrent.thread;
  * @author Zhang Peng
  */
 public class ThreadDemo01 {
+
     public static void main(String[] args) {
         Thread01 mt1 = new Thread01("线程A "); // 实例化对象
         Thread01 mt2 = new Thread01("线程B "); // 实例化对象
         mt1.run(); // 调用线程主体
         mt2.run(); // 调用线程主体
     }
-}
 
+    static class Thread01 extends Thread {
 
+        private int ticket = 5;
 
+        Thread01(String name) {
+            super(name);
+        }
 
-class Thread01 extends Thread {
-    private int ticket = 5;
-
-    Thread01(String name) {
-        super(name);
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < 100; i++) {
-            if (this.ticket > 0) {
-                System.out.println(this.getName() + " 卖票：ticket = " + ticket--);
+        @Override
+        public void run() {
+            for (int i = 0; i < 100; i++) {
+                if (this.ticket > 0) {
+                    System.out.println(this.getName() + " 卖票：ticket = " + ticket--);
+                }
             }
         }
     }
 }
+
+
+
--- codes/concurrent/src/main/java/io/github/dunwu/javase/concurrent/thread/ThreadDemo03.java
+++ codes/concurrent/src/main/java/io/github/dunwu/javase/concurrent/thread/ThreadDemo03.java
@@ -5,29 +5,32 @@ package io.github.dunwu.javase.concurrent.thread;
  * @author Zhang Peng
  */
 public class ThreadDemo03 {
+
     public static void main(String[] args) {
         Thread03 mt1 = new Thread03("线程A "); // 实例化对象
         mt1.start(); // 调用线程主体
         mt1.start(); // 调用线程主体
     }
-}
 
+    static class Thread03 extends Thread {
 
+        private int ticket = 5;
 
+        Thread03(String name) {
+            super(name);
+        }
 
-class Thread03 extends Thread {
-    private int ticket = 5;
-
-    Thread03(String name) {
-        super(name);
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < 100; i++) {
-            if (this.ticket > 0) {
-                System.out.println(this.getName() + " 卖票：ticket = " + ticket--);
+        @Override
+        public void run() {
+            for (int i = 0; i < 100; i++) {
+                if (this.ticket > 0) {
+                    System.out.println(this.getName() + " 卖票：ticket = " + ticket--);
+                }
             }
         }
     }
 }
+
+
+
+
