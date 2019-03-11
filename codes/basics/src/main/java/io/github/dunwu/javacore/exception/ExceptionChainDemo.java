a9eafa6e6f1ec143f477780ee7ad777a7c237958
Zhang Peng <forbreak@163.com>
2019-03-11 19:00:23 +0800

    异常和装箱拆箱示例

---
+++ codes/basics/src/main/java/io/github/dunwu/javacore/exception/ExceptionChainDemo.java
@@ -0,0 +1,36 @@
+package io.github.dunwu.javacore.exception;
+
+/**
+ * @author Zhang Peng
+ * @date 2019-03-11
+ */
+public class ExceptionChainDemo {
+    static class MyException1 extends Exception {
+        public MyException1(String message) {
+            super(message);
+        }
+    }
+
+
+    static class MyException2 extends Exception {
+        public MyException2(String message, Throwable cause) {
+            super(message, cause);
+        }
+    }
+
+    public static void f1() throws MyException1 {
+        throw new MyException1("出现 MyException1");
+    }
+
+    public static void f2() throws MyException2 {
+        try {
+            f1();
+        } catch (MyException1 e) {
+            throw new MyException2("出现 MyException2", e);
+        }
+    }
+
+    public static void main(String[] args) throws MyException2 {
+        f2();
+    }
+}
---
+++ codes/basics/src/main/java/io/github/dunwu/javacore/exception/FinallyOverrideExceptionDemo.java
@@ -0,0 +1,25 @@
+package io.github.dunwu.javacore.exception;
+
+/**
+ * @author Zhang Peng
+ * @date 2019-03-11
+ */
+public class FinallyOverrideExceptionDemo {
+    static void f() throws Exception {
+        try {
+            throw new Exception("A");
+        } catch (Exception e) {
+            throw new Exception("B");
+        } finally {
+            throw new Exception("C");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            f();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
