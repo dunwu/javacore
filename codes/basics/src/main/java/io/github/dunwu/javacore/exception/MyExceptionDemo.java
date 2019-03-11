1b27d6053dac1288a4a6a8196a975f54aa3c49ca
Zhang Peng <forbreak@163.com>
2019-03-11 14:29:18 +0800

    更新异常示例

---
+++ codes/basics/src/main/java/io/github/dunwu/javacore/exception/MyExceptionDemo.java
@@ -0,0 +1,18 @@
+package io.github.dunwu.javacore.exception;
+
+/**
+ * 自定义异常示例
+ * @author Zhang Peng
+ * @date 2019-03-11
+ */
+public class MyExceptionDemo {
+    public static void main(String[] args) {
+        throw new MyException("自定义异常");
+    }
+
+    static class MyException extends RuntimeException {
+        public MyException(String message) {
+            super(message);
+        }
+    }
+}
---
+++ codes/basics/src/main/java/io/github/dunwu/javacore/exception/RuntimeExceptionDemo.java
@@ -0,0 +1,10 @@
+package io.github.dunwu.javacore.exception;
+
+public class RuntimeExceptionDemo {
+    public static void main(String[] args) {
+        // 此处产生了异常
+        int result = 10 / 0;
+        System.out.println("两个数字相除的结果：" + result);
+        System.out.println("----------------------------");
+    }
+};
