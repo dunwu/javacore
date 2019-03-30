7e4c6499a8a0a85fda23acba8f0e73057c6c48d4
Zhang Peng <forbreak@163.com>
2019-03-31 00:19:48 +0800

    更新示例

---
+++ codes/basics/src/main/java/io/github/dunwu/javacore/annotation/InternalAnnotationDemo.java
@@ -0,0 +1,45 @@
+package io.github.dunwu.javacore.annotation;
+
+/**
+ * @author Zhang Peng
+ * @date 2019-03-30
+ */
+@SuppressWarnings({"uncheck", "deprecation"})
+public class InternalAnnotationDemo {
+
+    /**
+     * @SuppressWarnings 标记消除当前类的告警信息
+     */
+    @SuppressWarnings({"deprecation"})
+    static class A {
+        public void method1() {
+            System.out.println("call method1");
+        }
+
+        /**
+         * @Deprecated 标记当前方法为废弃方法，不建议使用
+         */
+        @Deprecated
+        public void method2() {
+            System.out.println("call method2");
+        }
+    }
+
+    /**
+     * @Deprecated 标记当前类为废弃类，不建议使用
+     */
+    @Deprecated
+    static class B extends A {
+        /**
+         * @Override 标记显示指明当前方法覆写了父类或接口的方法
+         */
+        @Override
+        public void method1() { }
+    }
+
+    public static void main(String[] args) {
+        A obj = new B();
+        obj.method1();
+        obj.method2();
+    }
+}
---
+++ codes/basics/src/main/java/io/github/dunwu/javacore/annotation/SuppressWarningsAnnotationDemo.java
@@ -0,0 +1,28 @@
+package io.github.dunwu.javacore.annotation;
+
+/**
+ * "@SuppressWarnings" 示例
+ * @author Zhang Peng
+ * @date 2019-03-30
+ */
+@SuppressWarnings({"rawtypes", "unchecked"})
+public class SuppressWarningsAnnotationDemo {
+    static class SuppressDemo<T> {
+        private T value;
+
+        public T getValue() {
+            return this.value;
+        }
+
+        public void setValue(T var) {
+            this.value = var;
+        }
+    }
+
+    @SuppressWarnings({"deprecation"})
+    public static void main(String[] args) {
+        SuppressDemo d = new SuppressDemo();
+        d.setValue("南京");
+        System.out.println("地名：" + d.getValue());
+    }
+}
